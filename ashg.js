8833dee58311b3f6556767d2e32c13265f06b7d4
taxocan <165591399+noneymtaxo@users.noreply.github.com>
2025-04-19 06:31:13 +0300

    Create ashg.js

---
+++ ashg.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  checkNamespam: true,
+  checkSelfbot: true
+});
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} olarak giriş yapıldı.`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.id === client.user.id) return; // Kendi mesajına tepki verme
+
+  if (message.content.toLowerCase() === 'sa') {
+    message.channel.send('as hoşgeldin.');
+  }
+});
+
+client.login('tokeniniz');
